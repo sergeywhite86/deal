a76d387dd94daa731169d8fe667ac06abcd1d346
Sergey White <sergey_white86@mail.ru>
2024-11-26 12:05:11 +0300

    init commit

---
+++ src/test/java/sergey_white/org/deal/DealApplicationTests.java
@@ -0,0 +1,13 @@
+package sergey_white.org.deal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
